e501a04ebe0eb87a3a8c5a01c7c3654e0f40bcb6
zhengyc <1625098483@qq.com>
2024-11-18 15:32:06 +0800

    component changes

--- jira-clone-dir/src/app/page.tsx
+++ jira-clone-dir/src/app/page.tsx
@@ -1,13 +1,33 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { TestComponent } from "@/features/test";
 
 
 export default function Home() {
   return (
-    <div>
-      <Button size={"sm"}>Click me </Button>
-      <p className="text-red-500 font-semibold">
-        Zheng
-      </p>
+    <div className="">
+      <Input />
+      <Button size={"lg"}>
+        Primary
+      </Button>
+      <Button variant={"secondary"}>
+        secondary
+      </Button>
+      <Button variant={"destructive"}>
+        destructive
+      </Button>
+      <Button variant={"ghost"}>
+        ghost
+      </Button>
+      <Button variant={"muted"}>
+        muted
+      </Button>
+      <Button variant={"outline"}>
+        outline
+      </Button>
+      <Button variant={"teritary"}>
+        teritary
+      </Button>
     </div>
   );
 }
