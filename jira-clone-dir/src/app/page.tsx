686f905bd52ad3c13a4bb225b29dea95bfaa621e
zhengyc <1625098483@qq.com>
2024-11-18 20:31:45 +0800

    accomplish sign up and sign in

--- jira-clone-dir/src/app/page.tsx
+++ jira-clone-dir/src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { TestComponent } from "@/features/test";
+
 
 
 export default function Home() {
