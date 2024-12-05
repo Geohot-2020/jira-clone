f577646d4da4dcc28fc74d716f7a31bae0efcd28
zhengyc <1625098483@qq.com>
2024-12-05 17:44:56 +0800

    完成新建任务

--- jira-clone-dir/src/features/tasks/components/task-view-switcher.tsx
+++ jira-clone-dir/src/features/tasks/components/task-view-switcher.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { DottedSeparator } from "@/components/dotted-separator";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { PlusIcon } from "lucide-react";
+import { useCreateTaskModal } from "../hooks/use-create-task-modal";
 
 
 export const TaskViewSwitcher = () => {
+    const { open } = useCreateTaskModal();
+
     return (
         <Tabs
             className="flex-1 w-full border rounded-lg"
@@ -32,16 +37,17 @@ export const TaskViewSwitcher = () => {
                         </TabsTrigger>
                     </TabsList>
                     <Button
+                        onClick={open}
                         size={"sm"}
                         className="w-full lg:w-auto"
                     >
-                        <PlusIcon className="w-full mr-2"/>
+                        <PlusIcon className="w-full mr-2" />
                         New
                     </Button>
                 </div>
-                <DottedSeparator className="my-4"/>
+                <DottedSeparator className="my-4" />
                 Data filter
-                <DottedSeparator className="my-4"/>
+                <DottedSeparator className="my-4" />
                 <>
                     <TabsContent value="table" className="mt-0">
                         Data table
---
+++ jira-clone-dir/src/features/tasks/hooks/use-create-task-modal.ts
@@ -0,0 +1,18 @@
+import { useQueryState, parseAsBoolean } from "nuqs";
+// 自定义钩子，用于管理创建工作区模态框的打开和关闭状态
+export const useCreateTaskModal = () => {
+    const [isOpen, setIsOpen] = useQueryState(
+        "create-task",
+        parseAsBoolean.withDefault(false).withOptions({clearOnDefault: true}),
+    );
+
+    const open = () => setIsOpen(true);
+    const close = () => setIsOpen(false);
+
+    return {
+        isOpen, 
+        open,
+        close,
+        setIsOpen,
+    }
+};
