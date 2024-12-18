2f87052c669077f437cfc2cadc98f4c23ae6d8b9
zhengyc <1625098483@qq.com>
2024-12-18 19:18:21 +0800

    增加项目分析功能

--- jira-clone-dir/src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
+++ jira-clone-dir/src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/client.tsx
@@ -8,45 +8,47 @@ import { useProjectId } from "@/features/projects/hooks/use-project-id";
 import { useGetProject } from "@/features/projects/api/use-get-project";
 import { PageLoader } from "@/components/page-loader";
 import { PageError } from "@/components/page-error";
-
-
+import { UseGetProjectAnalytics } from "@/features/projects/api/use-get-project-analytics";
+import { Analytics } from "@/components/analytics";
 
 
 export const ProjectClient = () => {
     const projectId = useProjectId();
-    const { data, isLoading } = useGetProject({ projectId });
+    const { data: project, isLoading: isLoadingProject } = useGetProject({ projectId });
+    const { data: analytics, isLoading: isLoadingAnalytics } = UseGetProjectAnalytics({ projectId });
+
+    const isLoading = isLoadingProject || isLoadingAnalytics;
 
     if (isLoading) {
         return <PageLoader />
     }
 
-    if (!data) {
+    if (!project || !analytics) {
         return <PageError message="Project not found" />
     }
-
-
 
     return (
         <div className="flex flex-col gap-y-4">
             <div className="flex items-center justify-between">
                 <div className="flex items-center gap-x-2">
                     <ProjectAvatar
-                        name={data.name}
-                        image={data.imageUrl}
+                        name={project.name}
+                        image={project.imageUrl}
                         className="size-8"
                     />
-                    <p className="text-lg font-semibold">{data.name}</p>
+                    <p className="text-lg font-semibold">{project.name}</p>
                 </div>
                 <div>
                     {/* 作为子元素提供 */}
                     <Button variant={"secondary"} size={"sm"} asChild>
-                        <Link href={`/workspaces/${data.workspaceId}/projects/${data.$id}/settings`}>
+                        <Link href={`/workspaces/${project.workspaceId}/projects/${project.$id}/settings`}>
                             <PencilIcon className="size-4 mr-2" />
                             Edit Project
                         </Link>
                     </Button>
                 </div>
             </div>
+            {analytics ? (<Analytics data={analytics} />) : null}
             <TaskViewSwitcher hideProjectFilter />
         </div>
     );
